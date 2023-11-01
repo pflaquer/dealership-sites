591ead8be84695be30ec4ddd8a70bf258bb5d21e
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-01 11:12:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+//import reactLogo from "./assets/react.svg";
 import "./App.css";
 import MainMenu from "./components/MainMenu/MainMenu";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/home/Home";
-import Results from "./components/results/Results";
-import Footer from "./components/Footer/Footer";
+//import Home from "./components/home/Home";
+//import Results from "./components/results/Results";
+//import Footer from "./components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,9 +13,6 @@ function App() {
   return (
     <Router>
       <MainMenu />
-      <Home />
-      <Results />
-      <Footer />
     </Router>
   );
 }
