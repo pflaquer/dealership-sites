fd2ced515065a7ef702a8624ef344d6183899a0f
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-01 11:34:43 -0400

    Create MainMenu.tsx

---
+++ src/components/MainMenu/MainMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo_.webp";
+import { NavLink } from "react-router-dom";
+
+function MainMenu() {
+return (
+        <>
+            <h1>Hello, world!</h1>
+            <p>This is a paragraph.</p>
+        </>
+    )
+}
+
+export default MainMenu;
