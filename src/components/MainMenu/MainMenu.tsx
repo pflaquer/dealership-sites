ddf1710912ea2bec11341df3f67633862337e6b5
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-01 12:05:37 -0400

    Update MainMenu.tsx

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -4,10 +4,37 @@ import { NavLink } from "react-router-dom";
 function MainMenu() {
 return (
         <>
-            <h1>Hello, world!</h1>
-            <p>This is a paragraph.</p>
-        </>
-    )
-}
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blogs">{used()}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">About Us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Call CTA</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+                </>
+  );
+};
 
 export default MainMenu;
