2780035e1210755ed33fbde904d5cdee89e0c2aa
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-01 11:01:50 -0400

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,14 +1,12 @@
 import { Box } from "@mui/material";
 import React from "react";
-import CarGallery from "../car/CarGallery";
-import { cars } from "../data/carsData";
+//import CarGallery from "../car/CarGallery";
+//import { cars } from "../data/carsData";
 
 function Home() {
   return (
     <Box sx={{ height: "calc(100vh-80px)", maxWidth:'100vw' }}>
-      {cars.slice(0, 1).map((car) => (
-        <CarGallery key={car.id} car={car} />
-      ))}
+      Hello World
     </Box>
   );
 }
